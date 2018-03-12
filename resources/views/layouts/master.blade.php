39f0ef36ffd22b57ce22d564cfeb34ac8c9963b0
ABDOULAYE DJIBRIL <koubia8@gmail.com>
2018-03-12 15:50:56 +0000

    first commit projet keneryapp

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="images/favicon_1.ico">
+
+        <title>SMART</title>
+
+        <!-- Base Css Files -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+        <!-- Font Icons -->
+        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+        <link href="assets/ionicon/css/ionicons.min.css" rel="stylesheet" />
+        <link href="css/material-design-iconic-font.min.css" rel="stylesheet">
+
+        <!-- animate css -->
+        <link href="css/animate.css" rel="stylesheet" />
+
+        <!-- Waves-effect -->
+        <link href="css/waves-effect.css" rel="stylesheet">
+
+        <!-- sweet alerts -->
+        <link href="assets/sweet-alert/sweet-alert.min.css" rel="stylesheet">
+
+        <!-- Custom Files -->
+        <link href="css/helper.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <script src="js/modernizr.min.js"></script>
+        
+    </head>
+
+
+
+    <body class="fixed-left">
+        
+        <!-- Begin page -->
+        <div id="wrapper">
+  @include('layouts.header.header')
+  @include('layouts.sidebar.sidebar')
+	<div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container">
+        @yield('content')
+
+
+     </div> <!-- container -->
+                               
+                </div> <!-- content -->
+                <footer class="footer text-right">
+                    2015 © Moltran.
+                </footer>
+</div>
+</div>
+
+      <!-- End wrapper -->
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/waves.js"></script>
+        <script src="js/wow.min.js"></script>
+        <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+        <script src="js/jquery.scrollTo.min.js"></script>
+        <script src="assets/chat/moment-2.2.1.js"></script>
+        <script src="assets/jquery-sparkline/jquery.sparkline.min.js"></script>
+        <script src="assets/jquery-detectmobile/detect.js"></script>
+        <script src="assets/fastclick/fastclick.js"></script>
+        <script src="assets/jquery-slimscroll/jquery.slimscroll.js"></script>
+        <script src="assets/jquery-blockui/jquery.blockUI.js"></script>
+
+        <!-- sweet alerts -->
+        <script src="assets/sweet-alert/sweet-alert.min.js"></script>
+        <script src="assets/sweet-alert/sweet-alert.init.js"></script>
+
+        <!-- flot Chart -->
+        <script src="assets/flot-chart/jquery.flot.js"></script>
+        <script src="assets/flot-chart/jquery.flot.time.js"></script>
+        <script src="assets/flot-chart/jquery.flot.tooltip.min.js"></script>
+        <script src="assets/flot-chart/jquery.flot.resize.js"></script>
+        <script src="assets/flot-chart/jquery.flot.pie.js"></script>
+        <script src="assets/flot-chart/jquery.flot.selection.js"></script>
+        <script src="assets/flot-chart/jquery.flot.stack.js"></script>
+        <script src="assets/flot-chart/jquery.flot.crosshair.js"></script>
+
+        <!-- Counter-up -->
+        <script src="assets/counterup/waypoints.min.js" type="text/javascript"></script>
+        <script src="assets/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+        
+        <!-- CUSTOM JS -->
+        <script src="js/jquery.app.js"></script>
+
+        <!-- Dashboard -->
+        <script src="js/jquery.dashboard.js"></script>
+
+        <!-- Chat -->
+        <script src="js/jquery.chat.js"></script>
+
+        <!-- Todo -->
+        <script src="js/jquery.todo.js"></script>
+
+        <script type="text/javascript">
+            /* ==============================================
+            Counter Up
+            =============================================== */
+            jQuery(document).ready(function($) {
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 1200
+                });
+            });
+        </script>
+	@yield('script')
+</body>
+</html>
+
